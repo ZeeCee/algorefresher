2a77311619360f9f579fb46b09c5419521c05dc3
zc <sinozhangch@gmail.com>
2014-12-21 03:25:36 +0800

    Reverse the sorting order

--- insertsort.py
+++ insertsort.py
@@ -5,15 +5,16 @@ insertion sort in reverse order: start sorting by placing the largest element at
 
 
 def insert_sort(inlist):
-	pos = len(inlist) -1  #start from the last item ( or the 2nd last also fine)
+	size = len(inlist)
+	pos = len(inlist) -2  #start from the 2nd last also fine)
 	while(pos>=0):
-		pivt = pos -1		#check through each item before the current position
-		while(pivt >= 0):	
-			if inlist[pivt]>inlist[pos]:
-				temp = inlist[pos]
-				inlist[pos] = inlist[pivt]
-				inlist[pivt]= temp
-			pivt -= 1
+		pivt = pos		#check through each item before the current position
+		while( pivt < size-1):	
+			if inlist[pivt]>inlist[pivt+1]:
+				maxi = inlist[pivt]
+				inlist[pivt]= inlist[pivt + 1]
+				inlist[pivt + 1] = maxi
+			pivt += 1
 		pos -= 1
 	return inlist
 
